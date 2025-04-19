852e64566b648ff3dadb0186c42593a70fe8085d
Tharun Raj <tharun.2.linux@gmail.com>
2025-04-19 15:04:38 +0530

    move to roboto as default fonts, lots of changes to various stuff

--- src/app.js
+++ src/app.js
@@ -4,3 +4,6 @@ const navbarLinks = document.querySelector(".navbar__links");
 navbarToggle.addEventListener("click", () => {
   navbarLinks.classList.toggle("menuOpen");
 });
+
+let footerYear = document.querySelector("#footerYear");
+footerYear.textContent = new Date().getFullYear();
